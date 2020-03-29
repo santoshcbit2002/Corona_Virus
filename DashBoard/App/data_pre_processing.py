c6d23435b495dfaded1826fee2995cb17ee38075
santoshcbit2002 <43035801+santoshcbit2002@users.noreply.github.com>
2020-03-29 16:52:01 +0100

    Add files via upload

---
+++ DashBoard/App/data_pre_processing.py
@@ -0,0 +1,180 @@
+import pandas as pd
+import numpy as np
+import warnings
+import time
+from geopy.geocoders import Nominatim
+import datetime
+import os
+import plotly.graph_objs as go
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+
+warnings.filterwarnings("ignore")
+print('Current Working Directory : ',os.getcwd())
+print('#'*60)
+print(' Data Preprocessing Script Started {}'.format(datetime.datetime.now()))
+
+confirmed=pd.read_csv('time_series_covid19_confirmed_global.csv')
+deaths=pd.read_csv('time_series_covid19_deaths_global.csv')
+recovered=pd.read_csv('time_series_covid19_recovered_global.csv')
+
+print(' *** Data load completed *** ')
+# Confirmed Cases Data - Processing
+confirmed.drop(columns=['Province/State','Lat','Long'],inplace=True)
+cols=list(confirmed)
+cols.remove('Country/Region')
+confirmed_country_group=confirmed.groupby(['Country/Region'])[cols].sum().reset_index()
+
+print(' *** Data pre-prossing on confirmed cases completed *** ')
+
+# Recovery Cases Data - Processing
+recovered.drop(columns=['Province/State','Lat','Long'],inplace=True)
+cols=list(recovered)
+cols.remove('Country/Region')
+recovered_country_group=recovered.groupby(['Country/Region'])[cols].sum().reset_index()
+
+print(' *** Data pre-prossing on recovered cases completed *** ')
+
+# Deaths Cases Data - Processing
+deaths.drop(columns=['Province/State','Lat','Long'],inplace=True)
+cols=list(deaths)
+cols.remove('Country/Region')
+deaths_country_group=deaths.groupby(['Country/Region'])[cols].sum().reset_index()
+
+print(' *** Data pre-prossing on death cases completed *** ')
+
+# Create Date list
+date_list=list(confirmed_country_group)
+date_list.remove('Country/Region')
+
+date_list_formatted=[]
+for i in date_list:
+    date_list_formatted.append((datetime.datetime.strptime(i, '%m/%d/%y')).date().isoformat())
+
+max_date=max(date_list_formatted)
+
+print(' *** Latest Date of the Data: ', max_date)
+print(' *** Starting the Transformation of Data *** ')
+
+# Transformation of Confirmed Cases Data 
+countries_list=confirmed_country_group['Country/Region'].unique()
+col_tran=list(confirmed_country_group)
+col_tran.remove('Country/Region')
+
+confirmed_cases_frame=pd.DataFrame()
+for i in  countries_list:
+    data_points=confirmed_country_group[confirmed_country_group['Country/Region']==i][col_tran].values
+    transformed_data=pd.DataFrame(data={'Country':i,
+                      'Dates':col_tran,
+                    'Confirmed':data_points[0]})
+    confirmed_cases_frame=confirmed_cases_frame.append(transformed_data)
+
+confirmed_cases_frame['Dates']=pd.to_datetime(confirmed_cases_frame['Dates'],format='%m/%d/%y')
+confirmed_cases_frame['Dates']=confirmed_cases_frame['Dates'].apply(lambda x:x.date())
+
+print(' *** Transformation of confirmed_cases data completed *** ')
+
+# Transformation of Recovered Data
+recovered_list=recovered_country_group['Country/Region'].unique()
+col_tran=list(recovered_country_group)
+col_tran.remove('Country/Region')
+
+recovered_cases_frame=pd.DataFrame()
+for i in  recovered_list:
+    data_points=recovered_country_group[recovered_country_group['Country/Region']==i][col_tran].values
+    transformed_data=pd.DataFrame(data={'Country':i,
+                      'Dates':col_tran,
+                    'Recovered':data_points[0]})
+    recovered_cases_frame=recovered_cases_frame.append(transformed_data)
+
+recovered_cases_frame['Dates']=pd.to_datetime(recovered_cases_frame['Dates'],format='%m/%d/%y')
+recovered_cases_frame['Dates']=recovered_cases_frame['Dates'].apply(lambda x:x.date())
+
+print(' *** Transformation of recovered_cases data completed *** ')
+
+# Transformation of Deaths Data
+
+deaths_list=deaths_country_group['Country/Region'].unique()
+col_tran=list(deaths_country_group)
+col_tran.remove('Country/Region')
+
+deaths_cases_frame=pd.DataFrame()
+for i in  deaths_list:
+    data_points=deaths_country_group[deaths_country_group['Country/Region']==i][col_tran].values
+    transformed_data=pd.DataFrame(data={'Country':i,
+                      'Dates':col_tran,
+                    'Deaths':data_points[0]})
+    deaths_cases_frame=deaths_cases_frame.append(transformed_data)
+
+deaths_cases_frame['Dates']=pd.to_datetime(deaths_cases_frame['Dates'],format='%m/%d/%y')
+deaths_cases_frame['Dates']=deaths_cases_frame['Dates'].apply(lambda x:x.date())
+
+print(' *** Transformation of deaths_cases data completed *** ')
+print(' *** Transformation of datasets completed *** ')
+# Create data required for latest date
+
+deaths_max_date=deaths_cases_frame[deaths_cases_frame['Dates']==deaths_cases_frame['Dates'].max()]
+recovered_max_date=recovered_cases_frame[recovered_cases_frame['Dates']==recovered_cases_frame['Dates'].max()]
+confirmed_max_date=confirmed_cases_frame[confirmed_cases_frame['Dates']==confirmed_cases_frame['Dates'].max()]
+
+# Set up Latitudes and Longitudes
+print(' *** Inititating Geo Py to set up Latitutes and Logitudes *** ')
+country_list=confirmed_cases_frame['Country'].unique()
+
+def do_geocode(x):
+    geopy = Nominatim()
+    try:
+        return geopy.geocode(x)
+    except GeocoderTimedOut:
+        return do_geocode(x)
+
+vals=[]
+keys=[]
+start_time=datetime.datetime.now()
+print(' *** Geo Py coding Starting *** ')
+
+for i in country_list:
+    location=do_geocode(i)
+    keys.append(i)
+    vals.append([location.latitude,location.longitude])
+
+print(' *** Geo Py code Ended. total elapsed time {} *** '.format(start_time-datetime.datetime.now()))
+
+dictionary=dict(zip(keys,vals))
+def latitude_extractor(x):
+    return dictionary[x][0]
+def longitude_extractor(x):
+    return dictionary[x][1]
+
+confirmed_cases_frame['latitude']=confirmed_cases_frame['Country'].map(latitude_extractor)
+confirmed_cases_frame['longitudes']=confirmed_cases_frame['Country'].map(longitude_extractor)
+
+confirmed_cases_frame['Dates']=pd.to_datetime(confirmed_cases_frame['Dates'],format='%Y-%m-%d')
+confirmed_cases_temp=confirmed_cases_frame[confirmed_cases_frame['Confirmed']>0]
+confirmed_cases_timeline=confirmed_cases_temp.sort_values('Dates').groupby('Country').head(1)
+confirmed_cases_timeline.sort_values(['Dates'],inplace=True)
+confirmed_cases_timeline['Date_id']=confirmed_cases_timeline.groupby('Dates').ngroup()
+
+print(' *** Geo Py coding ended successfully. Saving the Files *** ')
+
+# Save all the files 
+# Save Confirmed, Received and Death cases  (# Plots - 1,2,3 and 5 - Time line curves)
+
+confirmed_cases_frame.to_csv('pre_processed_confirmed_cases.csv')
+recovered_cases_frame.to_csv('pre_processed_recovered_cases.csv')
+deaths_cases_frame.to_csv('pre_processed_deaths_cases.csv')
+
+# Save Confirmed, Received and Death cases for Max date  (# Plot 4)
+
+confirmed_max_date.to_csv('pre_processed_confirmed_max_date.csv')
+recovered_max_date.to_csv('pre_processed_recovered_max_date.csv')
+deaths_max_date.to_csv('pre_processed_deaths_max_date.csv')
+
+
+# Save Confirmed, Received and Death cases for Max date  (# Plot 6 - Map)
+
+confirmed_cases_timeline.to_csv('pre_processed_confirmed_cases_timeline.csv')
+
+
+print(' All the Files Saved. Program Ended Successfully *** ')
---
+++ DashBoard/App/data_pull.py
@@ -0,0 +1,80 @@
+# Import Libraries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import datetime as dt
+import re
+import os
+
+# Define Constants
+url_qualifier='https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series/'
+file_qualifier_confirmed='Time_Series_Confirmed'
+file_qualifier_deaths='Time_Series_Deaths'
+file_qualifier_recovered='Time_Series_Recovered'
+confirmed_file='time_series_covid19_confirmed_global'
+deaths='time_series_covid19_deaths_global'
+recovered='time_series_covid19_recovered_global'
+extension='.csv'
+file_list=[confirmed_file,deaths,recovered]
+
+
+
+# Define helper methods
+def get_url_data(date_val):
+    start=dt.datetime.now()
+    url = url_qualifier + date_val + extension
+    
+    response = requests.get(url)
+    if str(response)=='<Response [200]>':
+          soup= BeautifulSoup(response.text, 'html.parser')
+    else:
+          print('get_url failed. Bad response received : {}'.format(str(response)))
+    print('Total Execution Time {}'.format(dt.datetime.now()-start))
+    return soup    
+
+def extract_headers(soup_data):
+    html_text=soup_data.findAll('tr')
+    listed_html_text=list(html_text)
+    print('Length of listed html text: {}'.format(len(listed_html_text)))
+    cols=[]
+    b=list(listed_html_text[0])
+    for i in range(0,len(b)):
+        if str(b[i])!= '\n':
+            node=re.match("<td class=*",str(b[i]))
+            if not node:
+                value=re.match('<th>(.*)</th>',str(b[i])).group(1)
+                cols.append(value)
+    return cols
+
+def extract_data(soup_data):
+    data_array=[]
+    html_text=soup_data.findAll('tr')
+    listed_html_text=list(html_text)
+    for j in range(1,len(listed_html_text)):
+        b=list(listed_html_text[j])
+        d=[]
+        for i in range(0,len(b)):
+            if str(b[i])!= '\n':
+                node=re.match("<td class=*",str(b[i]))
+                if not node:
+                    value=re.match('<td>(.*)</td>',str(b[i])).group(1)
+                    d.append(value)
+        data_array.append(d)
+    return data_array
+
+print('Current working Directory : ' + os.getcwd())
+
+# Define Main method
+if __name__ == "__main__":
+
+    for k in file_list:  
+        print('Data extraction started for {}'.format(k))   
+        soup=get_url_data(k)
+        cols=extract_headers(soup)
+        data_array=extract_data(soup)
+        data = pd.DataFrame(data_array, columns=cols)
+        filename=k+extension
+        data.to_csv(filename,encoding='utf-8',index=False)
+        print('Data extraction complete for Date {}. Shape of data {}.'.format(k,data.shape))
+        time.sleep(60) 
